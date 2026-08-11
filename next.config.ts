8959253d4972363a2f014d8468616119f5ed1cbd
Pamela Terrell <pammyhoney@yahoo.com>
2026-08-11 14:16:54 -0400

    config to remove -testing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/cases/michael-bargo-testing",
+        destination: "/cases/michael-bargo",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
